08ff496c369ebacd6c9272c232900aea25d7c459
liulihui <17600100962@163.com>
2018-12-20 00:13:44 +0800

    我见青山多妩媚

--- 05fs/fs01.js
+++ 05fs/fs01.js
@@ -1,1 +1,48 @@
-console.log("addsfsdf");
+// var fs = require("fs");
+// fs.stat("html",(err,stats)=>{
+//     if(err){
+//         console.log(err);
+//         return false;
+//     }
+//     console.log("文件:"+stats.isFile());
+//     console.log("目录:"+stats.isDirectory());
+// })
+var fs = require("fs");
+// fs.stat("index.txt",(err,stats)=>{
+//     if(err){
+//         console.log(err);
+//         return false;
+//     }
+//     console.log("文件:"+stats.isFile());
+//     console.log("目录:"+stats.isDirectory());
+// });
+
+// fs.mkdir("css",(err)=>{
+//     if(err){
+//         console.log(err);
+//         return false;
+//     }
+//     console.log("创建目录成功");
+// })
+// fs.writeFile('t.txt',"你好nodejs",function(err){
+//     if(err){
+//         console.log(err);
+//         return false;
+//     }
+//     console.log("写入成功");
+// })
+// fs.appendFile('t1.txt','这是追加的内容',function(err){
+//     if(err){
+//         console.log(err);
+//         return false;
+//     }
+//     console.log("追加成功");
+// })
+fs.readFile('index.txt',function(err,data){
+        if(err){
+        console.log(err);
+        return false;
+        }
+         console.log(data.toString());
+        //  console.log(data);
+})
---
+++ 05fs/fs02.js
@@ -0,0 +1,18 @@
+var fs = require("fs");
+
+// fs.stat("upload",function(err,stats){
+//     if(err){
+//         // console.log(err)
+//         fs.mkdir("upload",function(err1){
+//             if(err1){
+//                 console.log(err1)
+//                 return false;
+//             }else{
+//                 console.log("创建upload目录成功")
+//             }
+//         })
+//     }else{
+//         console.log("目录已经存在");
+//         console.log(stats.isDirectory());
+//     }
+// })
